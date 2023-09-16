869436fe3f7fb7d7204cf3f1a055f1f09dfa44b3
Sobhan <imsobhanmoradi@gmail.com>
2023-09-16 14:39:36 +0430

    part #12

--- assets/js/app.js
+++ assets/js/app.js
@@ -5,31 +5,47 @@ document.addEventListener('DOMContentLoaded', function () {
     });
 
 
-    console.log(1);
+    // console.log(1);
+
+    // new Promise(
+    //     function (resolve, reject) {
+    //         setTimeout(function () {
+    //             console.log(2);
+    //             resolve("OK")
+    //             reject("Not OK ...")
+    //         }, 1000)
+    //     }
+
+    // ).then(
+    //     function (response) {
+    //         console.log(response);
+    //         console.log(3);
+    //     }
+    // ).catch(
+    //     function (error) {
+    //         console.log(error);
+    //     }
+    // )
+
+    // console.log(4)
+    // console.log(5)
 
 
-    new Promise(
-        function (resolve, reject) {
-            setTimeout(function () {
-                console.log(2);
-                resolve("OK")
-                reject("Not OK ...")
-            }, 1000)
-        }
 
-    ).then(
-        function (response) {
-            console.log(response);
-            console.log(3);
-        }
-    ).catch(
-        function (error) {
-            console.log(error);
-        }
-    )
 
-    console.log(4)
-    console.log(5)
+
+
+
+
+
+
+
+
+
+
+    // fetch('https://jsonplaceholder.typicode.com/users')
+    //     .then((response) => response.json())
+    //     .then((json) => console.log(json));
 
 
 });
